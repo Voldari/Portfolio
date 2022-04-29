5fc80df899eb4af85d185afaddfcfb311b4249e8
voldari <isaacharper@gmail.com>
2022-04-29 09:50:02 -0700

    build

---
+++ .vercel_build_output/static/_app/pages/articles/trailer_first_look.svelte-ce1597d0.js
@@ -0,0 +1,1 @@
+import{S as t,i as e,s as l}from"../../chunks/index-c127a35e.js";class r extends t{constructor(s){super(),e(this,s,null,null,l,{})}}export{r as default};
--- src/lib/blocks/Article.svelte
+++ src/lib/blocks/Article.svelte
@@ -1,26 +1,20 @@
 <script>
+
     
 </script>
 
 <p>
-    <slot name="date"><!-- optional fallback --></slot>
+    <slot name="date"></slot>
 </p>
 <h1>
-    <slot name="title"><!-- optional fallback --></slot>
+    <slot name="title"></slot>
 </h1>
 <hr>
 <div class="container">
-    <p>{data.text1}</p>
-    <h2>{data.heading1}</h2>
-    <p>{data.text2}</p>
-    <p>{data.text3}</p>
-    <p>{data.text4}</p>
-    <h2>{data.heading2}</h2>
-    <p>{data.text5}</p>
-    <p>{data.text6}</p>
+
 </div>
 
-<slot name="body"><!-- optional fallback --></slot>
+<slot name="body"></slot>
 
 <style>
     h1 { 
